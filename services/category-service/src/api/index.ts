ee0e53f2d863966cc167898580d59328d58a9580
3aSoftwares <3asoftwares@gmail.com>
2026-01-31 22:29:53 +0530

    Revert commits on origin/main to restore code at 086f66998ec79812f712a751486a56580aa54e06

--- services/category-service/src/api/index.ts
+++
@@ -1,5 +0,0 @@
-import serverless from 'serverless-http';
-import app from '../index';
-
-// Export a serverless-compatible handler for Vercel/other platforms
-export default serverless(app as any);
